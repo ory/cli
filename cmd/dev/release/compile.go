79f8f5d1accfdcf991b6d5a125c70eb6f1f97315
aeneasr <aeneas@ory.sh>
2020-07-09 18:28:18 +0200

    feat: add xgoreleaser compile helpers

---
+++ cmd/dev/release/compile.go
@@ -0,0 +1,34 @@
+package release
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/ory/x/flagx"
+	"github.com/spf13/cobra"
+
+	"github.com/ory/cli/cmd/pkg"
+)
+
+var compile = &cobra.Command{
+	Use:   "compile",
+	Args:  cobra.ExactArgs(0),
+	Short: "Compiles the current project using oryd/xgoreleaser a new release",
+	Run: func(cmd *cobra.Command, args []string) {
+		wd, err := os.Getwd()
+		pkg.Check(err)
+
+		pkg.Check(pkg.NewCommand("docker", "run", "--mount",
+			fmt.Sprintf(`type=bind,source=%s,target=/project`, wd),
+			"oryd/xgoreleaser:"+flagx.MustGetString(cmd, "tag"),
+			"--skip-publish", "--snapshot", "--rm-dist", "--parallelism",
+			strconv.Itoa(flagx.MustGetInt(cmd,"parallelism"))).Run())
+	},
+}
+
+func init() {
+	Main.AddCommand(compile)
+	compile.Flags().StringP("tag", "t", "1.14.4-0.139.0", "Set the xgoreleaser version tag.")
+	compile.Flags().IntP("parallelism", "p", 4, "Build parallelism.")
+}
