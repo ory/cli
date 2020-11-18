c4efd81cba047c5881b202f5819f2d30737a282e
aeneasr <3372410+aeneasr@users.noreply.github.com>
2020-11-18 15:56:46 +0100

    fix: apply code review

--- cmd/dev/newsletter/draft.go
+++ cmd/dev/newsletter/draft.go
@@ -88,6 +88,18 @@ func Draft(listID string, segmentID int, tagMessageRaw, changelogRaw []byte) (*g
 		segmentOptions.SavedSegmentId = segmentID
 	}
 
+	brandColor := "#5528FF"
+	switch strings.ToLower(repoName) {
+	case "oathkeeper":
+		brandColor = "#BD2FEF"
+	case "hydra":
+		brandColor = "#FF6A85"
+	case "kratos":
+		brandColor = "#FF9800"
+	case "keto":
+		brandColor = "#1DE9B6"
+	}
+
 	var body bytes.Buffer
 	pkg.Check(readTemplate(pkger.Open("/view/mail-body.html")).Execute(&body, struct {
 		Version     string
@@ -96,6 +108,7 @@ func Draft(listID string, segmentID int, tagMessageRaw, changelogRaw []byte) (*g
 		RepoName    string
 		Changelog   template.HTML
 		Message     template.HTML
+		BrandColor  string
 	}{
 		Version:     tag,
 		GitTag:      tag,
@@ -103,6 +116,7 @@ func Draft(listID string, segmentID int, tagMessageRaw, changelogRaw []byte) (*g
 		RepoName:    repoName,
 		Changelog:   changelog,
 		Message:     tagMessage,
+		BrandColor:  brandColor,
 	}))
 
 	chimp := gochimp3.New(chimpKey)
