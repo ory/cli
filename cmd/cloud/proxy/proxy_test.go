a968cbdd7b1780f37f0df175ed9d855e07ea3918
hackerman <3372410+aeneasr@users.noreply.github.com>
2021-02-24 13:11:58 +0100

    feat: add ory cloud proxy (#58)

---
+++ cmd/cloud/proxy/proxy_test.go
@@ -0,0 +1,233 @@
+package proxy_test
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"github.com/ory/cli/cmd"
+	"github.com/ory/cli/cmd/cloud/proxy"
+	"github.com/ory/cli/cmd/cloud/remote"
+	"github.com/ory/herodot"
+	"github.com/ory/x/cmdx"
+	"github.com/ory/x/logrusx"
+	"github.com/ory/x/urlx"
+	"github.com/phayes/freeport"
+	"github.com/pkg/errors"
+	"github.com/square/go-jose/v3"
+	"github.com/square/go-jose/v3/jwt"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/tidwall/gjson"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"os"
+	"strconv"
+	"strings"
+	"testing"
+	"time"
+)
+
+const (
+	anonymous = "anonymous"
+)
+
+var (
+	session = json.RawMessage(`{
+  "id": "821f5a53-a0b3-41fa-9c62-764560fa4406",
+  "active": true,
+  "expires_at": "2021-02-25T09:25:37.929792Z",
+  "authenticated_at": "2021-02-24T09:25:37.931774Z",
+  "issued_at": "2021-02-24T09:25:37.929813Z",
+  "identity": {
+	"id": "18aafd3e-b00c-4b19-81c8-351e38705126",
+	"schema_id": "default",
+	"schema_url": "https://example.projects.oryapis.com/api/kratos/public/schemas/default",
+	"traits": {
+	  "email": "foo@bar"
+	}
+  }
+}`)
+)
+
+func newCommand(t *testing.T, ctx context.Context) *cmdx.CommandExecuter {
+	return &cmdx.CommandExecuter{New: cmd.NewRootCmd, Ctx: ctx}
+}
+
+func newUpstream(t *testing.T, keyURL string, writer herodot.Writer) *httptest.Server {
+	upstream := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		parts := strings.Split(r.Header.Get("Authorization"), " ")
+		if len(parts) == 1 {
+			_, _ = w.Write([]byte(anonymous))
+			return
+		}
+
+		res, err := http.Get(keyURL)
+		if err != nil {
+			writer.WriteError(w, r, errors.WithStack(err))
+			return
+		}
+		defer res.Body.Close()
+
+		var keys jose.JSONWebKeySet
+		require.NoError(t, json.NewDecoder(res.Body).Decode(&keys))
+
+		tok, err := jwt.ParseSigned(parts[1])
+		if err != nil {
+			writer.WriteError(w, r, errors.WithStack(err))
+			return
+		}
+
+		var claims map[string]interface{}
+		if err := tok.Claims(keys.Keys[0].Key, &claims); err != nil {
+			writer.WriteError(w, r, errors.WithStack(err))
+			return
+		}
+
+		writer.Write(w, r, claims)
+	}))
+
+	t.Cleanup(upstream.Close)
+	return upstream
+}
+
+func cloudAPi(t *testing.T, writer herodot.Writer) *url.URL {
+	router := httprouter.New()
+	router.GET("/api/kratos/public/sessions/whoami", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		if r.Header.Get("Authorization") != "ory" {
+			writer.WriteError(w, r, errors.WithStack(herodot.ErrUnauthorized))
+			return
+		}
+
+		writer.Write(w, r, session)
+	})
+
+	router.GET("/api/kratos/public/self-service/login/browser", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		_, _ = w.Write([]byte("login"))
+	})
+
+	api := httptest.NewServer(router)
+	t.Cleanup(api.Close)
+	parsed, err := url.ParseRequestURI(api.URL)
+	require.NoError(t, err)
+	return parsed
+}
+
+func getRequest(t *testing.T, href string) ([]byte, *http.Response) {
+	res, err := http.DefaultClient.Get(href)
+	require.NoError(t, err)
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	require.NoError(t, err)
+	return body, res
+}
+
+func assertAnonymous(t *testing.T, href string) {
+	body, _ := getRequest(t, href)
+	assert.EqualValues(t, string(body), anonymous, "%s", body)
+}
+
+func TestProxy(t *testing.T) {
+	port, err := freeport.GetFreePort()
+	require.NoError(t, err)
+	proxyURL := fmt.Sprintf("http://127.0.0.1:%d", port)
+
+	l := logrusx.New("ory cli", "tests")
+	writer := herodot.NewJSONWriter(l)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	t.Cleanup(cancel)
+
+	upstream := newUpstream(t, proxyURL+"/.ory/jwks.json", writer)
+	cloudApi := cloudAPi(t, writer)
+	ctx = context.WithValue(ctx, remote.FlagEndpoint, cloudApi)
+
+	go func() {
+		stdout, stderr, err := newCommand(t, ctx).Exec(os.Stdin, "proxy", upstream.URL, "--"+remote.FlagProject, "demo", "--"+proxy.PortFlag, fmt.Sprintf("%d", port), "--allow-anonymous", "/public/1", "--allow-anonymous", "/public/2")
+		assert.ErrorIs(t, err, context.Canceled)
+		t.Logf("stdout:\n%s", stdout)
+		t.Logf("stderr:\n%s", stderr)
+	}()
+
+	var tries int
+	for {
+		time.Sleep(time.Second)
+
+		tries++
+		if tries > 30 {
+			t.Fatal("Proxy did not come alive")
+		}
+
+		res, err := http.Get(proxyURL + "/.ory/jwks.json")
+		if err != nil {
+			t.Logf("Proxy is not yet alive: %s", err)
+			continue
+		}
+		_ = res.Body.Close()
+
+		if res.StatusCode != http.StatusOK {
+			t.Logf("Proxy is not yet alive: %d is not 200", res.StatusCode)
+			continue
+		}
+
+		break
+	}
+
+	t.Run("allows anonymous paths", func(t *testing.T) {
+		assertAnonymous(t, proxyURL+"/public/1")
+		assertAnonymous(t, proxyURL+"/public/2")
+	})
+
+	t.Run("forwards the request if authenticated", func(t *testing.T) {
+		req, _ := http.NewRequest("GET", proxyURL+"/private", nil)
+		req.Header.Set("Authorization", "ory")
+		res, err := http.DefaultClient.Do(req)
+		require.NoError(t, err)
+		defer res.Body.Close()
+
+		body, err := ioutil.ReadAll(res.Body)
+		require.NoError(t, err)
+
+		assert.EqualValues(t, http.StatusOK, res.StatusCode)
+		assert.EqualValues(t, gjson.GetBytes(session, "identity.id").String(), gjson.GetBytes(body, "sub").String())
+		assert.EqualValues(t, urlx.AppendPaths(cloudApi).String(), gjson.GetBytes(body, "iss").String())
+	})
+
+	t.Run("responds with 401 json if json request", func(t *testing.T) {
+		req, _ := http.NewRequest("GET", proxyURL+"/private", nil)
+		req.Header.Set("Accept", "application/json")
+		res, err := http.DefaultClient.Do(req)
+		require.NoError(t, err)
+		defer res.Body.Close()
+
+		body, err := ioutil.ReadAll(res.Body)
+		require.NoError(t, err)
+
+		assert.EqualValues(t, http.StatusUnauthorized, res.StatusCode)
+		assert.EqualValues(t, "The provided credentials are expired, malformed, missing, or otherwise invalid.", gjson.GetBytes(body, "error.reason").String(), "%s", body)
+	})
+
+	t.Run("responds with 302 redirect to login if HTML request", func(t *testing.T) {
+		for k, accept := range []string{} {
+			t.Run("case="+strconv.Itoa(k), func(t *testing.T) {
+				req, _ := http.NewRequest("GET", proxyURL+"/private", nil)
+				if accept != "" {
+					req.Header.Set("Accept", accept)
+				}
+
+				res, err := http.DefaultClient.Do(req)
+				require.NoError(t, err)
+				defer res.Body.Close()
+
+				body, err := ioutil.ReadAll(res.Body)
+				require.NoError(t, err)
+
+				assert.EqualValues(t, http.StatusOK, res.StatusCode)
+				assert.EqualValues(t, urlx.AppendPaths(cloudApi, "/api/kratos/public/self-service/login/browser").String(), res.Request.URL.String())
+				assert.EqualValues(t, "The provided credentials are expired, malformed, missing, or otherwise invalid.", gjson.GetBytes(body, "reason").String(), "%s", body)
+			})
+		}
+	})
+}
--- go.mod
+++ go.mod
@@ -12,23 +12,32 @@ require (
 	github.com/Masterminds/semver/v3 v3.1.1
 	github.com/avast/retry-go v3.0.0+incompatible
 	github.com/deckarep/golang-set v1.7.1
+	github.com/elnormous/contenttype v0.0.0-20210110050721-79150725153f
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/gobuffalo/fizz v1.13.1-0.20201104174146-3416f0e6618f
 	github.com/gobuffalo/pop/v5 v5.3.2-0.20210128124218-e397a61c1704
+	github.com/gobuffalo/x v0.0.0-20181007152206-913e47c59ca7
+	github.com/gofrs/uuid/v3 v3.1.2
 	github.com/gomarkdown/markdown v0.0.0-20201113031856-722100d81a8e
 	github.com/google/uuid v1.1.5
 	github.com/jackc/pgx/v4 v4.10.1
+	github.com/julienschmidt/httprouter v1.2.0
 	github.com/markbates/pkger v0.17.1
 	github.com/ory/gochimp3 v0.0.0-20200417124117-ccd242db3655
+	github.com/ory/graceful v0.1.1
+	github.com/ory/herodot v0.9.0
 	github.com/ory/jsonschema/v3 v3.0.2
 	github.com/ory/kratos v0.5.5-alpha.1.0.20210222162419-48e5b9302d17
 	github.com/ory/kratos-client-go v0.5.4-alpha.1
-	github.com/ory/x v0.0.195
+	github.com/ory/x v0.0.198
+	github.com/phayes/freeport v0.0.0-20180830031419-95f893ade6f2
 	github.com/pkg/errors v0.9.1
 	github.com/spf13/cobra v1.1.3
 	github.com/spf13/pflag v1.0.5
+	github.com/square/go-jose/v3 v3.0.0-20200630053402-0a67ce9b0693
 	github.com/stretchr/testify v1.7.0
 	github.com/tidwall/gjson v1.6.7
 	github.com/tidwall/sjson v1.1.4
+	github.com/urfave/negroni v1.0.0
 	gopkg.in/yaml.v2 v2.4.0
 )
