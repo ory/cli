bf7a1a907624cc6e85ed4cfbfaf01692c4c5ee39
zepatrik <zepatrik@users.noreply.github.com>
2021-09-21 17:11:33 +0200

    feat(dev): pre-release hooks closes #106

--- cmd/pkg/repos.go
+++ cmd/pkg/repos.go
@@ -1,19 +1,48 @@
 package pkg
 
 import (
-	"path"
+	"gopkg.in/yaml.v2"
+	"os"
 
-	"github.com/ory/x/stringslice"
+	"github.com/ory/x/stringsx"
 )
 
-var supported = []string{"hydra", "kratos", "keto", "oathkeeper", "cli"}
+type (
+	Config struct {
+		Project         oryProject `yaml:"project"`
+		PreReleaseHooks []string   `yaml:"pre_release_hooks"`
+	}
+	oryProject string
+)
 
-func ProjectFromDir(dir string) string {
-	project := path.Base(dir)
-	if !stringslice.Has(supported, project) {
-		Fatalf(`This command is expected to run in a directory named "hydra", "keto", "oathkeeper", "kratos".`)
-		return ""
+func ReadConfig() (*Config, error) {
+	raw, err := os.ReadFile(".orycli.yml")
+	if err != nil {
+		return nil, err
 	}
 
-	return project
+	var c Config
+	if err := yaml.Unmarshal(raw, &c); err != nil {
+		return nil, err
+	}
+	return &c, nil
+}
+
+func (p *oryProject) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	var raw string
+	if err := unmarshal(&raw); err != nil {
+		return err
+	}
+
+	switch r := stringsx.SwitchExact(raw); {
+	case r.AddCase("hydra"),
+		r.AddCase("kratos"),
+		r.AddCase("keto"),
+		r.AddCase("oathkeeper"),
+		r.AddCase("cli"):
+		*p = oryProject(raw)
+	default:
+		return r.ToUnknownCaseErr()
+	}
+	return nil
 }
