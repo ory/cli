79c87caf62c555f701891ece2bb72301a862dd1c
aeneasr <3372410+aeneasr@users.noreply.github.com>
2021-11-26 08:25:50 +0000

    chore: update repository templates
    [skip ci] - updated repository templates to https://github.com/ory/meta/commit/57d31bfbd55b2371112a0d5b2acaacc407340c82

--- cypress/integration/proxy.spec.js
+++ cypress/integration/proxy.spec.js
@@ -2,7 +2,6 @@ const randomString = () => (Math.random() + 1).toString(36).substring(7)
 const randomPassword = () => randomString() + randomString()
 const randomEmail = () => randomString() + '@' + randomString() + '.com'
 
-
 const login = (email, password) => {
   cy.visit('/.ory/ui/login')
   cy.get('[name="password_identifier"]').type(email)
@@ -21,17 +20,17 @@ describe('ory proxy', () => {
   const email = randomEmail()
   const password = randomPassword()
   before(() => {
-    cy.clearCookies({domain: null})
-  });
+    cy.clearCookies({ domain: null })
+  })
 
   it('navigation works', () => {
-    cy.visit('/.ory/ui/registration');
+    cy.visit('/.ory/ui/registration')
     cy.get('.card-action a').click()
     cy.location('pathname').should('eq', '/.ory/ui/login')
   })
 
   it('should be able to execute registration', () => {
-    cy.visit('/.ory/ui/registration');
+    cy.visit('/.ory/ui/registration')
     cy.get('[name="traits.email"]').type(email)
     cy.get('[name="password"]').type(password)
     cy.get('[name="method"]').click()
@@ -44,21 +43,28 @@ describe('ory proxy', () => {
     cy.request('/anything').should((res) => {
       expect(res.body.headers['Authorization']).to.not.be.empty
       const token = res.body.headers['Authorization'].replace(/bearer /gi, '')
-      console.log({token})
+      console.log({ token })
 
-      cy.task('verify', res.body.headers['Authorization'].replace(/bearer /gi, '')).then((decoded) => {
+      cy.task(
+        'verify',
+        res.body.headers['Authorization'].replace(/bearer /gi, '')
+      ).then((decoded) => {
         expect(decoded.session.identity.traits.email).to.equal(email)
       })
-    });
+    })
   })
 
   it('should be able to execute logout', () => {
     login(email, password)
     cy.visit('/.ory/ui/welcome')
-    cy.get('[data-testid="logout"]').should('have.attr', 'aria-disabled', 'false')
+    cy.get('[data-testid="logout"]').should(
+      'have.attr',
+      'aria-disabled',
+      'false'
+    )
     cy.get('[data-testid="logout"]').click()
     cy.request('/anything').should((res) => {
       expect(res.body.headers['Authorization']).to.be.undefined
-    });
+    })
   })
 })
