715683476fd2945044381b3172cc9eb6d8fafe63
Kevin Goslar <kevin.goslar@gmail.com>
2022-06-30 09:15:05 -0500

    license.go license_test.go

--- cmd/dev/headers/license.go
+++ cmd/dev/headers/license.go
@@ -22,15 +22,17 @@ type FormatFunc func(text string) string
 
 // formatFuncs lists all formatFuncs known to this tool.
 var formatFuncs = map[string]FormatFunc{
-	"yml":  ymlComment,
-	"yaml": ymlComment,
+	"yml":  YmlComment,
+	"yaml": YmlComment,
 }
 
-// addLicenses adds or updates the Ory license header
-// in all files of the current directory or its subdirectories.
-func addLicenses(dir string, year int) error {
+// addLicenses adds or updates the Ory license header in all files within the given directory.
+func AddLicenses(dir string, year int) error {
 	licenseText := fmt.Sprintf(LICENSE_TEMPLATE, year)
-	filepath.Walk(".", func(path string, info fs.FileInfo, err error) error {
+	filepath.Walk(dir, func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			return fmt.Errorf("cannot traverse directory %s: %w", path, err)
+		}
 		if info.IsDir() {
 			// filepath.Walk traverses subdirectories
 			return nil
@@ -41,27 +43,40 @@ func addLicenses(dir string, year int) error {
 			// not a file that we can add comments to --> nothing to do here
 			return nil
 		}
-		file, err := os.OpenFile(path, os.O_RDWR, 0755)
+		file, err := os.OpenFile(path, os.O_RDWR, 0744)
+		if err != nil {
+			return fmt.Errorf("cannot open file %s: %w", path, err)
+		}
+		defer file.Close()
 		buffer := make([]byte, info.Size())
 		count, err := file.Read(buffer)
 		if err != nil {
 			return fmt.Errorf("cannot read file %s: %w", path, err)
 		}
-		if count != int(info.Size()) {
-			return fmt.Errorf("did not read the entire file")
+		if int64(count) != info.Size() {
+			return fmt.Errorf("did not read the entire %d bytes of file %s but only %d", info.Size(), path, count)
 		}
 		fileContent := string(buffer)
-		fileContentWithoutHeader := removeHeader(fileContent, commentFunc, LICENSE_TOKEN)
-		header := commentFunc(licenseText)
-		fileContentWithNewHeader := fmt.Sprintf("%s\n%s", header, fileContentWithoutHeader)
-		file.Truncate(0)
-		file.WriteString(fileContentWithNewHeader)
-		return file.Close()
+		fileContentNoHeader := Remove(fileContent, commentFunc, LICENSE_TOKEN)
+		newHeader := commentFunc(licenseText)
+		fileContentNewHeader := fmt.Sprintf("%s\n%s", newHeader, fileContentNoHeader)
+		err = file.Truncate(0)
+		if err != nil {
+			return fmt.Errorf("cannot truncate file %s: %w", path, err)
+		}
+		count, err = file.WriteString(fileContentNewHeader)
+		if err != nil {
+			return fmt.Errorf("cannot write file %s: %w", path, err)
+		}
+		if count != len(fileContentNewHeader) {
+			return fmt.Errorf("did not write the entire %d bytes into %s but only %d, file is corrupted now", len(fileContentNewHeader), path, count)
+		}
+		return nil
 	})
 	return nil
 }
 
-func removeHeader(text string, commentFunc FormatFunc, token string) (result string) {
+func Remove(text string, commentFunc FormatFunc, token string) (result string) {
 	commentWithToken := commentFunc(token)
 	inComment := false
 	for _, line := range strings.Split(text, "\n") {
@@ -79,23 +94,8 @@ func removeHeader(text string, commentFunc FormatFunc, token string) (result str
 	return result
 }
 
-// addHeaderToFile adds the given header to the given file
-func addHeaderToFile(header string, file string) error {
-	return nil
-}
-
-// licenseText provides the complete text for the license.
-func licenseText(text string, year int) {
-	fmt.Sprintf(LICENSE_TEMPLATE, year)
-}
-
-// comment provides a valid multi-line comment for the given filetype containing the given text.
-func comment(text, filetype string, year int) (string, error) {
-	return "", nil
-}
-
 // commentForYML provides a YML comment containing the given text
-func ymlComment(text string) (result string) {
+func YmlComment(text string) (result string) {
 	for _, line := range strings.Split(text, "\n") {
 		result += fmt.Sprintf("# %s\n", line)
 	}
@@ -108,7 +108,7 @@ var copyright = &cobra.Command{
 	Args:  cobra.ExactArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
 		year, _, _ := time.Now().Date()
-		return addLicenses(args[0], year)
+		return AddLicenses(args[0], year)
 	},
 }
 
--- cmd/dev/headers/license_test.go
+++ cmd/dev/headers/license_test.go
@@ -1,9 +1,13 @@
-package headers
+package headers_test
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"testing"
 
+	"github.com/ory/cli/cmd/dev/headers"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -15,7 +19,7 @@ func TestYmlComment(t *testing.T) {
 	for give, want := range tests {
 		t.Run(fmt.Sprintf("%s -> %s", give, want), func(t *testing.T) {
 			t.Parallel()
-			have := ymlComment(give)
+			have := headers.YmlComment(give)
 			assert.Equal(t, want, have)
 		})
 	}
@@ -29,6 +33,45 @@ name: test
 hello: world`
 	want := `name: test
 hello: world`
-	have := removeHeader(give, ymlComment, LICENSE_TOKEN)
+	have := headers.Remove(give, headers.YmlComment, "Copyright ©")
 	assert.Equal(t, want, have)
+}
+
+func TestAddLicenses(t *testing.T) {
+	dir := createTmpDir()
+	dir.createFile("one.yml", "one: two\nalpha: beta")
+	dir.createFile("two.yml", "three: four\ngamma: delta")
+	headers.AddLicenses(dir.path, 2022)
+	assert.Equal(t, "# Copyright © 2022 Ory Corp Inc.\n\none: two\nalpha: beta\n", dir.content("one.yml"))
+	assert.Equal(t, "# Copyright © 2022 Ory Corp Inc.\n\nthree: four\ngamma: delta\n", dir.content("two.yml"))
+}
+
+// HELPERS
+
+// a directory used for testing, no need to clean up
+type testDir struct {
+	path string
+}
+
+func createTmpDir() testDir {
+	path, err := ioutil.TempDir("", "ory-license")
+	if err != nil {
+		panic(err)
+	}
+	return testDir{path}
+}
+
+func (t testDir) createFile(name, content string) {
+	err := os.WriteFile(filepath.Join(t.path, name), []byte(content), 0744)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (t testDir) content(path string) string {
+	content, err := os.ReadFile(filepath.Join(t.path, path))
+	if err != nil {
+		panic(err)
+	}
+	return string(content)
 }
