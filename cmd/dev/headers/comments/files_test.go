811f165e109480d017eeecf1c3b7ae4f6d9faaf3
Kevin Goslar <kevin.goslar@gmail.com>
2022-07-25 11:21:25 -0500

    files_test.go

--- cmd/dev/headers/comments/files_test.go
+++ cmd/dev/headers/comments/files_test.go
@@ -10,30 +10,30 @@ import (
 
 func TestFileContentWithoutHeader_knownFile(t *testing.T) {
 	give := "<!-- copyright Ory -->\n<!-- all rights reserved -->\n\nhello world"
+	want := "hello world"
 	createTestFile(t, "testfile.md", give)
 	defer os.Remove("testfile.md")
 	have, err := comments.FileContentWithoutHeader("testfile.md", "copyright")
-	want := "hello world"
 	assert.NoError(t, err)
 	assert.Equal(t, want, have)
 }
 
 func TestFileContentWithoutHeader_otherCommentFirst(t *testing.T) {
 	give := "<!-- another comment -->\n\n<!-- copyright Ory -->\n<!-- all rights reserved -->\n\nhello world"
+	want := "<!-- another comment -->\n\nhello world"
 	createTestFile(t, "testfile.md", give)
 	defer os.Remove("testfile.md")
 	have, err := comments.FileContentWithoutHeader("testfile.md", "copyright")
-	want := "<!-- another comment -->\n\nhello world"
 	assert.NoError(t, err)
 	assert.Equal(t, want, have)
 }
 
 func TestFileContentWithoutHeader_unknownFile(t *testing.T) {
 	give := "hello world"
+	want := "hello world"
 	createTestFile(t, "testfile.txt", give)
 	defer os.Remove("testfile.txt")
 	have, err := comments.FileContentWithoutHeader("testfile.txt", "copyright")
-	want := "hello world"
 	assert.NoError(t, err)
 	assert.Equal(t, want, have)
 }
