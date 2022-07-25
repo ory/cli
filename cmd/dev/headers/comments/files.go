fb2c4272d700028a35e3b061f954662d15384ba9
Kevin Goslar <kevin.goslar@gmail.com>
2022-07-25 11:08:41 -0500

    files.go

--- cmd/dev/headers/comments/files.go
+++ cmd/dev/headers/comments/files.go
@@ -21,7 +21,7 @@ func FileContentWithoutHeader(path, token string) (string, error) {
 }
 
 // WriteFileWithHeader creates a file at the given path containing the given file content (header comment + body).
-// the header comment is supposed to contain only text. This method will transform it into a comment.
+// The header comment is supposed to contain only text. This method will transform it into a comment.
 func WriteFileWithHeader(path, header, body string) error {
 	file, err := os.Create(path)
 	if err != nil {
