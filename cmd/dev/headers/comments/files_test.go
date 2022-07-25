b4b3c17b7a3daa231e4b2dd2edd4ef886c641e84
Kevin Goslar <kevin.goslar@gmail.com>
2022-07-25 11:34:00 -0500

    files_test.go

--- cmd/dev/headers/comments/files_test.go
+++ cmd/dev/headers/comments/files_test.go
@@ -10,47 +10,47 @@ import (
 )
 
 func TestFileContentWithoutHeader_knownFile(t *testing.T) {
-	give := `
+	give := trim(`
 <!-- copyright Ory -->
 <!-- all rights reserved -->
 
-file content`
-	want := `
-file content`
-	createTestFile(t, "testfile.md", trim(give))
+file content`)
+	want := trim(`
+file content`)
+	createTestFile(t, "testfile.md", give)
 	defer os.Remove("testfile.md")
 	have, err := comments.FileContentWithoutHeader("testfile.md", "copyright")
 	assert.NoError(t, err)
-	assert.Equal(t, trim(want), have)
+	assert.Equal(t, want, have)
 }
 
 func TestFileContentWithoutHeader_otherCommentFirst(t *testing.T) {
-	give := `
+	give := trim(`
 <!-- another comment block -->
 
 <!-- copyright Ory -->
 <!-- all rights reserved -->
 
-file content`
-	want := `
+file content`)
+	want := trim(`
 <!-- another comment block -->
 
-file content`
-	createTestFile(t, "testfile.md", trim(give))
+file content`)
+	createTestFile(t, "testfile.md", give)
 	defer os.Remove("testfile.md")
 	have, err := comments.FileContentWithoutHeader("testfile.md", "copyright")
 	assert.NoError(t, err)
-	assert.Equal(t, trim(want), have)
+	assert.Equal(t, want, have)
 }
 
 func TestFileContentWithoutHeader_unknownFile(t *testing.T) {
 	give := "file content"
 	want := "file content"
-	createTestFile(t, "testfile.txt", trim(give))
+	createTestFile(t, "testfile.txt", give)
 	defer os.Remove("testfile.txt")
 	have, err := comments.FileContentWithoutHeader("testfile.txt", "copyright")
 	assert.NoError(t, err)
-	assert.Equal(t, trim(want), have)
+	assert.Equal(t, want, have)
 }
 
 // Helper function that indicates within a test that we create a test file
