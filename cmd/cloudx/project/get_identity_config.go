00fdcb302a9e7225aebbaed06a492151d3fa41e8
Kevin Goslar <kevin.goslar@gmail.com>
2022-09-01 11:15:41 -0500

    get_identity_config.go

--- cmd/cloudx/project/get_identity_config.go
+++ cmd/cloudx/project/get_identity_config.go
@@ -13,7 +13,7 @@ func NewGetKratosConfigCmd() *cobra.Command {
 		Aliases: []string{"ic", "kratos-config"},
 		Args:    cobra.ExactArgs(1),
 		Short:   "Get an Ory Cloud project's identity configuration",
-		Long:    "Get an Ory Cloud project's identity configuration. You can use this command to render Ory Kratos configurations as well.",
+		Long:    "Get an Ory Cloud or Ory Kratos project's identity configuration.",
 		Example: `$ ory get kratos-config ecaaa3cb-0730-4ee8-a6df-9553cdfeef89 --format yaml > kratos-config.yaml
 
 $ ory get kratos-config ecaaa3cb-0730-4ee8-a6df-9553cdfeef89 --format json
