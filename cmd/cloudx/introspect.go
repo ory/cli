e3b8824e6c81dbca79120fa7856a994c088cab72
aeneasr <3372410+aeneasr@users.noreply.github.com>
2022-10-25 11:04:41 +0200

    fix: introspect command

--- cmd/cloudx/introspect.go
+++ cmd/cloudx/introspect.go
@@ -3,6 +3,8 @@ package cloudx
 import (
 	"github.com/spf13/cobra"
 
+	"github.com/ory/cli/cmd/cloudx/client"
+
 	"github.com/ory/cli/cmd/cloudx/oauth2"
 	"github.com/ory/x/cmdx"
 )
@@ -14,6 +16,8 @@ func NewIntrospectCmd() *cobra.Command {
 	}
 	cmd.AddCommand(oauth2.NewIntrospectToken())
 
+	client.RegisterConfigFlag(cmd.PersistentFlags())
+	client.RegisterYesFlag(cmd.PersistentFlags())
 	cmdx.RegisterHTTPClientFlags(cmd.PersistentFlags())
 	cmdx.RegisterFormatFlags(cmd.PersistentFlags())
 	return cmd
