38f776844c30e865b67af975d0ad3aea045e26db
Til Schünemann <45126872+tilschuenemann@users.noreply.github.com>
2023-01-26 08:17:30 +0100

    feat: fix tunnel (#296)
    Closes #271
    
    Co-authored-by: hackerman <3372410+aeneasr@users.noreply.github.com>

--- cmd/cloudx/proxy/proxy.go
+++ cmd/cloudx/proxy/proxy.go
@@ -353,7 +353,6 @@ func checkOry(conf *config, _ *logrusx.Logger, writer herodot.Writer, keys *jose
 		}
 
 		session, err := checkSession(hc, r, endpoint)
-		r.Header.Del("Authorization")
 		if err != nil || !gjson.GetBytes(session, "active").Bool() {
 			next(w, r)
 			return
